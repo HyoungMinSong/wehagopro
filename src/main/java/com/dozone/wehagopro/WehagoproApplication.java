640100e9d934d65bcceca78748f08ea5c69d431a
HyoungMinSong <shmin11@naver.com>
2023-07-05 12:00:10 +0900

    Initial commit

---
+++ src/main/java/com/dozone/wehagopro/WehagoproApplication.java
@@ -0,0 +1,14 @@
+package com.dozone.wehagopro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class WehagoproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WehagoproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dozone/wehagopro/WehagoproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dozone.wehagopro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WehagoproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
