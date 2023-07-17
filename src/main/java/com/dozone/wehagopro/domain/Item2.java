6809bdc29074d60e756566044564869f7ced48a3
jongwon0801 <choijong0801@naver.com>
2023-07-17 16:20:04 +0900

    db연결, item2, xml 파라미터 수정

--- src/main/java/com/dozone/wehagopro/domain/Item2.java
+++ src/main/java/com/dozone/wehagopro/domain/Item2.java
@@ -3,26 +3,34 @@ package com.dozone.wehagopro.domain;
 import lombok.Data;
 @Data
 public class Item2 {
-    private int num;
-    private String id;
-    private String pw;
+
+    private String t_user_id;
+    private String t_user_password;
 
     public Item2() {
 
     }
 
 
-    public Item2(String name, String idd, String pw) {
+    public Item2(String userid, String password) {
 
-
-        this.id = id;
-        this.pw = pw;
+        this.t_user_id = userid;
+        this.t_user_password = password;
     }
 
-    public Item2(String id, String pw) {
-
-        this.id = id;
-        this.pw = pw;
+    public String getT_user_id() {
+        return t_user_id;
     }
 
+    public void setT_user_id(String t_user_id) {
+        this.t_user_id = t_user_id;
+    }
+
+    public String getT_user_password() {
+        return t_user_password;
+    }
+
+    public void setT_user_password(String t_user_password) {
+        this.t_user_password = t_user_password;
+    }
 }
--- src/main/java/com/dozone/wehagopro/domain/LoginForm.java
+++ src/main/java/com/dozone/wehagopro/domain/LoginForm.java
@@ -1,25 +1,14 @@
 package com.dozone.wehagopro.domain;
 
+import lombok.Data;
+
+@Data
 public class LoginForm {
-    private String username;
+    private String userid;
     private String password;
 
     // Getter와 Setter 메서드
     // ...
 
-    public String getUsername() {
-        return username;
-    }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
