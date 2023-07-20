3dfa95b0a888ad6dc5e9b2ec61c2085d476cd50d
Cometary <109892844+Cometary@users.noreply.github.com>
2023-07-20 15:18:48 +0900

    조직도 수정 기능 프로토타입 추가 DTO 이름명 수정

--- src/main/java/com/dozone/wehagopro/controller/OrganizationController.java
+++ src/main/java/com/dozone/wehagopro/controller/OrganizationController.java
@@ -1,10 +1,10 @@
 package com.dozone.wehagopro.controller;
 
-import com.dozone.wehagopro.domain.CountEmployee;
-import com.dozone.wehagopro.domain.User;
-import com.dozone.wehagopro.domain.WorkPlace;
+import com.dozone.wehagopro.domain.OrganizationEditDTO;
+import com.dozone.wehagopro.domain.OrganizationInitEmplDTO;
+import com.dozone.wehagopro.domain.UserDTO;
+import com.dozone.wehagopro.domain.OrganizationInitCompDTO;
 import com.dozone.wehagopro.service.OrganizationService;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,20 +19,29 @@ public class OrganizationController {
 
     // 조직도 목록
     @GetMapping("/showMyWorkPlace")
-    public List<WorkPlace> showMyWorkPlace(Integer t_user_no){
+    public List<OrganizationInitCompDTO> showMyWorkPlace(Integer t_user_no){
         return organizationService.showMyWorkPlace(t_user_no);
     }
 
     // 조직도 직원 상태
     @GetMapping("/showMyEmployeeState")
-    public CountEmployee showMyEmployeeState(Integer t_company_no){
+    public OrganizationInitEmplDTO showMyEmployeeState(Integer t_company_no){
         return organizationService.showMyEmployeeState(t_company_no);
     }
 
     // 조직도 직원 목록
     @GetMapping("/showMyEmployees")
-    public List<User> showMyEmployees(@RequestParam("nodeName") String nodeName, @RequestParam("pk") Integer pk, @RequestParam("index") Integer index, @RequestParam("t_employee_state") Integer t_employee_state){
+    public List<UserDTO> showMyEmployees(@RequestParam("nodeName") String nodeName, @RequestParam("pk") Integer pk, @RequestParam("index") Integer index, @RequestParam("t_employee_state") Integer t_employee_state){
         return organizationService.showMyEmployees(nodeName, pk, index, t_employee_state);
+    }
+
+    // 조직도 부서 수정
+    @PostMapping("/editingOrganization")
+    public void editingOrganization(@RequestBody List<OrganizationEditDTO> dto){
+        System.out.println(dto.get(0));
+        System.out.println(dto.get(0).getT_organization_name());
+        System.out.println(dto.get(0).getT_company_no());
+        System.out.println(dto.get(0).getT_organization_no());
     }
 
 }
