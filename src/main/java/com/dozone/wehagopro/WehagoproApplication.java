69c1ee58f781ef42c73c26481855788e80a8d5d8
이주용 <jylee0097@gmail.com>
2023-07-24 09:43:55 +0900

    로그인 컨트롤러 수정

--- src/main/java/com/dozone/wehagopro/WehagoproApplication.java
+++ src/main/java/com/dozone/wehagopro/WehagoproApplication.java
@@ -3,16 +3,13 @@ package com.dozone.wehagopro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class WehagoproApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WehagoproApplication.class, args);
-
-
-
-
 	}
 
 }
