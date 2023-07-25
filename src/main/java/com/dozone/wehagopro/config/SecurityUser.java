4331b47af5f5d1f90abd7304645be2692a5a5020
이주용 <jylee0097@gmail.com>
2023-07-25 12:53:49 +0900

    로그인 JWT 토큰 거의 마지막

--- src/main/java/com/dozone/wehagopro/config/SecurityUser.java
+++ src/main/java/com/dozone/wehagopro/config/SecurityUser.java
@@ -1,6 +1,6 @@
 package com.dozone.wehagopro.config;
 
-import com.dozone.wehagopro.domain.UserDto;
+import com.dozone.wehagopro.domain.UserDTO;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 
@@ -9,7 +9,7 @@ import java.util.Collections;
 public class SecurityUser extends User {
     private static final long serialVersionUID = 1L;
 
-    public SecurityUser(UserDto userDto) {
+    public SecurityUser(UserDTO userDto) {
         // 여기서 UserDetails 객체 생성
         super(userDto.getT_user_id(), userDto.getT_user_password(), Collections.emptyList());
     }
