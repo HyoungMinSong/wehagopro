f6ba69a4498cd625ecaabc964033f8b55fbbd10b
Cometary <109892844+Cometary@users.noreply.github.com>
2023-08-14 15:41:36 +0900

    Feat : 핸드폰 인증 구현중

---
+++ src/main/java/com/dozone/wehagopro/service/common/PhoneService.java
@@ -0,0 +1,25 @@
+package com.dozone.wehagopro.service.common;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class PhoneService {
+
+    public String sendRandomMessage(String tel) {
+        Naver_Sens_V2 message = new Naver_Sens_V2();
+        Random rand = new Random();
+        String numStr = "";
+        for (int i = 0; i < 6; i++) {
+            String ran = Integer.toString(rand.nextInt(10));
+            numStr += ran;
+        }
+        System.out.println("회원가입 문자 인증 => " + numStr);
+
+        message.send_msg(tel, numStr);
+
+        return numStr;
+    }
+
+}
